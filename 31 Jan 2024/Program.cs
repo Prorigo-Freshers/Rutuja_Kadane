95203d1d75f9873ce084c63a539f6c7c9f85f058
Rutuja Kadane <kadanerutuja@gmail.com>
2024-01-31 21:08:13 +0530

    31 jan

---
+++ 31 Jan 2024/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, result, sum = 0, temp;
+            Console.WriteLine("Enter the Number: ");
+            n = int.Parse(Console.ReadLine());
+            temp = n;
+            while (n > 0)
+            {
+                result = n % 10;
+                sum = (sum * 10) + result;
+                n = n / 10;
+            }
+            if (temp == sum)
+                Console.Write("Number is Palindrome.");
+            else
+                Console.Write("Number is not Palindrome");
+        }
+    }
+}
